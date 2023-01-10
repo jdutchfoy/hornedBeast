5225284ab1bbef4014aac0eb76feaf5139235a33
Dutch Foy <jdutchfoy@gmail.com>
2023-01-09 19:24:16 -0800

    footer app.js done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-// 1st import
+//1st import
 import React from 'react';
-import Header from "./Header";
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
-
-
-
-// 2nd Class component 
-class App extends React.Component{
+//2nd Class Component
+class App extends React.Component {
   render(){
     return(
       <>
-        <Header />
-        <h1>Horned Beasts</h1>
-        <h2>Hey!</h2>
+      <Header/>
+      <h2>Yo!</h2>
+      <Main/>
+      <Footer/>
       </>
-
     )
   }
 }
-  
-
-
-
 
 // #rd Export the component so other files can import them 
 export default App;
