1e5c03d68c6f83cfe4376f08f064f83ae3f10958
Dutch Foy <jdutchfoy@gmail.com>
2023-01-11 23:25:38 -0800

    initial commit day 3

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+## day-two
+
 Number and name of feature: ________________________________
 
 Estimate of time needed to complete: __?___
@@ -7,3 +9,17 @@ Start time: __3:00pm___
 Finish time: 10:40pm____
 
 Actual time needed to complete: _7pm____
+
+## day-three
+
+Number and name of feature: ________________________________
+
+Estimate of time needed to complete: __3 1/2___
+
+Start time: _2 4:40 10:45___
+
+Finish time: _____
+
+Actual time needed to complete: _____
+
+I wish I got my AH HAH moment.... 
--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
+import './Main.css';
 
 class Main extends React.Component{
   render() {
     return(
       <>
+      <main>
        <h2>Horned Beast</h2>
        <HornedBeast title={"Rhino Family"} image_url={'https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80",'} alt={"Parent Rhino with two babies"} description={"Parent Rhino with two babies"}/>
 
