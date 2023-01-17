ccbed9bd4aadd2a0035d23e0d7817244f0622c2a
T'Challa <jdutchfoy@gmail.com>
2023-01-16 19:08:02 -0800

    complete?

---
+++ src/Components/Main.js
@@ -0,0 +1,33 @@
+// 1st import
+import React from "react";
+import HornedBeast from "./components/HornedBeast.js";
+// import data from "./components/data.json";
+import "./Main.css";
+
+// class component
+class Main extends React.Component {
+  render() {
+    let hornedBeast = this.props.data.map(animals => {
+      
+      return <HornedBeast
+        title={animals.title}
+        src={animals.image_url}
+        description={animals.description}
+        key={animals._id}
+        addHearts={this.props.addHearts}
+        handleOpenModal={this.props.handleOpenModal}
+        horns={animals.horns}
+      />
+    });
+
+    return (
+      <>
+        <main>        
+          {hornedBeast}
+        </main>
+      </>
+    )
+  }
+}
+
+// export
