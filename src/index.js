3a113eccd64c50d6bc3138de94eb57c2722da6fb
dutch foy <jdutchfoy@gmail.com>
2023-01-28 19:06:36 -0800

    netlify error fix attempt app index reportWebVitals.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './Components/App';
-import reportWebVitals from './reportWebVitals';
+import App from './App';
+import reportWebVitals from './Components/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
